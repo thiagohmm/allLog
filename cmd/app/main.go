c179755831012d51859821bd9a9d7a4ca12e8c61
thiago.mileo <thiago.mileo@gnaos.com>
2025-01-29 15:26:50 -0300

    Criado main

---
+++ cmd/app/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/thiagohmm/allLog/configuration"
+	"github.com/thiagohmm/allLog/internal/service"
+)
+
+func LoadConfig() (*configuration.Conf, error) {
+	// Carrega as configurações do arquivo .env
+	cfg, err := configuration.LoadConfig("../../.env")
+	if err != nil {
+		log.Fatalf("Erro ao carregar configuração: %v", err)
+	}
+	return cfg, err
+}
+
+func main() {
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Erro ao carregar configuração: %v", err)
+	}
+
+	log.Printf("Configuração carregada:")
+
+	// Inicializa o serviço de mensagens
+	messageService := service.MessageService{}
+
+	messageService.ListenToQueue(context.Background(), cfg.ENV_RABBITMQ, "logs")
+
+}
